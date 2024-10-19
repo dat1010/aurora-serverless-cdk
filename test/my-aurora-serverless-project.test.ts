fa8b17a1c04f394dc827fedef0e2b324ece0f795
Tanner Jr., David Arthur <dtannerjr@bwh.harvard.edu>
2024-10-19 16:53:46 -0400

    Initial commit

---
+++ test/my-aurora-serverless-project.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as MyAuroraServerlessProject from '../lib/my-aurora-serverless-project-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/my-aurora-serverless-project-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new MyAuroraServerlessProject.MyAuroraServerlessProjectStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
