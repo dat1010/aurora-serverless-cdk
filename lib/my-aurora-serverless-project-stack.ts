e35c1a4c98c9a2f33736944ea03ce9c332690edd
Tanner Jr., David Arthur <dtannerjr@bwh.harvard.edu>
2024-10-20 17:13:01 -0400

    first commit

--- lib/my-aurora-serverless-project-stack.ts
+++ lib/my-aurora-serverless-project-stack.ts
@@ -1,16 +1,55 @@
 import * as cdk from 'aws-cdk-lib';
+
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as rds from 'aws-cdk-lib/aws-rds';
 
 export class MyAuroraServerlessProjectStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const vpc = new ec2.Vpc(this, 'Vpc', {
+      maxAzs: 2 // Default is all AZs in the region
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'MyAuroraServerlessProjectQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+
+    const databaseName = 'envirologix';
+    const dbSecurityGroup = new ec2.SecurityGroup(this, 'DbSecurityGroup', {
+      vpc,
+
+      allowAllOutbound: true,
+    });
+
+    // Allow incoming traffic from any IP on PostgreSQL port 5432
+    dbSecurityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(5432));
+
+
+    const dbCluster = new rds.ServerlessCluster(this, 'PortalAuroraProd', {
+      vpc,
+      engine: rds.DatabaseClusterEngine.AURORA_POSTGRESQL,
+
+      enableDataApi: true,
+
+      defaultDatabaseName: databaseName,
+      parameterGroup: rds.ParameterGroup.fromParameterGroupName(
+        this,
+        'ParameterGroup',
+
+        'default.aurora-postgresql13'
+
+      ),
+      scaling: {
+        minCapacity: rds.AuroraCapacityUnit.ACU_2, // default is 2 ACUs
+        maxCapacity: rds.AuroraCapacityUnit.ACU_2, // default is 16 ACUs
+      },
+      securityGroups: [dbSecurityGroup],
+      credentials: rds.Credentials.fromPassword('postgres', cdk.SecretValue.plainText('postgres')), // Set user and password
+    });
+
+    // Optional: Output the cluster endpoint
+    new cdk.CfnOutput(this, 'DBClusterEndpoint', {
+      value: dbCluster.clusterEndpoint.socketAddress,
+    });
   }
+
 }
